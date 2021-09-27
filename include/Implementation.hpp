55cbaa03c1e4ed0ab361128fa3e3d0bedbc8e54d
M F R Siam <mfrforhad1@outlook.com>
2021-09-27 22:56:07 +0600

    Some Codes From Bst Commited

--- include/Implementation.hpp
+++ include/Implementation.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <iostream>
+
+/*! @brif This Namespace Implements Binary Search Tree */
+namespace BST {
+	/* @brif Each Node Of the Tree
+		@param Value It Creats Node With the given Value with The next Pointer as a nullptr*/
+	struct Node {
+		int value;
+		Node* next;
+		Node() :value(0),next(nullptr){}
+		Node(int i):value(i),next(nullptr){}
+	};
+
+	class BST {
+
+	};
+}
---
+++ src/Source.cpp
@@ -0,0 +1,8 @@
+#include <fmt/ostream.h>
+#include "../include/Implementation.hpp"
+
+int main(void) {
+	fmt::print("Hello World");
+
+	return EXIT_SUCCESS;
+}
